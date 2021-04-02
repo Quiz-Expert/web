77dc2f5f4aa8785e4be270bd9a56ce5522d302bd
5david5 <48356242+5david5@users.noreply.github.com>
2021-04-02 22:43:33 +0200

    #11 - Create homepage

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createWebHistory } from 'vue-router'
+import translations from '../lang'
+import { createLangRouter } from 'vue-lang-router'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Homepage',
+    component: () => import("../views/Homepage"),
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
-const router = createRouter({
+const langRouterOptions = {
+	defaultLanguage: 'pl',
+	translations,
+}
+const routerOptions = {
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+}
+const router = createLangRouter(langRouterOptions, routerOptions)
 
 export default router
