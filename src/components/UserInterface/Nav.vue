a41b1fa29a0fa62166d97141bdef0f9fc75227f8
5david5 <48356242+5david5@users.noreply.github.com>
2021-04-14 21:31:42 +0200

    #29 - Update Nav

--- src/components/UserInterface/Nav.vue
+++ src/components/UserInterface/Nav.vue
@@ -9,23 +9,23 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <localized-link
-                to="/user/categories"
-                class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                :to="{ name: 'Categories' }"
+                class="nav-link text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 v-text="$t('pages.user.navigation.categories')"
               />
               <localized-link
-                to="/user/invitations"
-                class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                :to="{ name: 'Invitations' }"
+                class="nav-link text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 v-text="$t('pages.user.navigation.invitations')"
               />
               <localized-link
-                to="/user/friends"
-                class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                :to="{ name: 'Friends' }"
+                class="nav-link text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 v-text="$t('pages.user.navigation.friends')"
               />
               <localized-link
-                to="/user/profile"
-                class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                :to="{ name: 'Profile' }"
+                class="nav-link text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 v-text="$t('pages.user.navigation.profile')"
               />
             </div>
@@ -33,29 +33,27 @@
         </div>
         <div class="hidden md:block">
           <div class="ml-4 flex items-center md:ml-6">
-            <localized-link to="/user/notifications">
-              <button
-                type="button"
-                class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-yellow-500"
+            <button
+              type="button"
+              class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-yellow-500"
+            >
+              <span class="sr-only">{{ $t('pages.user.navigation.notifications') }}</span>
+              <svg
+                class="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
               >
-                <span class="sr-only">{{ $t('pages.user.navigation.notifications') }}</span>
-                <svg
-                  class="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                  />
-                </svg>
-              </button>
-            </localized-link>
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                />
+              </svg>
+            </button>
 
             <div class="ml-3 relative">
               <div>
@@ -102,13 +100,13 @@
                   aria-labelledby="user-menu"
                 >
                   <localized-link
-                    to="/user/settings"
+                    :to="{ name: 'Settings' }"
                     class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                     role="menuitem"
                     v-text="$t('pages.user.navigation.settings')"
                   />
                   <localized-link
-                    to="/"
+                    :to="{ name: 'Homepage' }"
                     class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                     role="menuitem"
                     v-text="$t('pages.user.navigation.sing-out')"
@@ -125,6 +123,7 @@
             aria-controls="mobile-menu"
             aria-expanded="false"
             @click="isOpenMenu = !isOpenMenu"
+            @focusout="isOpenMenu = false"
           >
             <span class="sr-only">{{ $t('pages.user.navigation.open-user-menu') }}</span>
             <svg
@@ -152,56 +151,63 @@
       </div>
     </div>
 
-    <div v-show="isOpenMenu" id="mobile-menu" class="md:hidden">
-      <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-        <localized-link
-          to="/user/categories"
-          class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
-          v-text="$t('pages.user.navigation.categories')"
-        />
-        <localized-link
-          to="/user/invitations"
-          class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          v-text="$t('pages.user.navigation.invitations')"
-        />
-        <localized-link
-          to="/user/friends"
-          class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          v-text="$t('pages.user.navigation.friends')"
-        />
-        <localized-link
-          to="/user/profile"
-          class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          v-text="$t('pages.user.navigation.profile')"
-        />
-      </div>
-      <div class="pt-4 pb-3 border-t border-gray-700">
-        <div class="flex items-center px-4 sm:px-6 lg:px-8">
-          <div class="flex-shrink-0 bg-gray-800 p-1 rounded-full text-gray-400">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-              />
-            </svg>
-          </div>
-          <div class="ml-3">
-            <div class="text-base font-medium leading-none text-white">Tom Cook</div>
-          </div>
+    <transition
+      enter-active-class="duration-200 ease-out"
+      enter-class="opacity-0 scale-95"
+      enter-to-class="opacity-100 scale-100"
+      leave-active-class="duration-100 ease-in"
+      leave-class="opacity-100 scale-100"
+      leave-to-class="opacity-0 scale-95"
+    >
+      <div v-show="isOpenMenu" id="mobile-menu" class="md:hidden">
+        <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+          <localized-link
+            :to="{ name: 'Categories' }"
+            class="nav-link text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            v-text="$t('pages.user.navigation.categories')"
+          />
+          <localized-link
+            :to="{ name: 'Invitations' }"
+            class="nav-link text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            v-text="$t('pages.user.navigation.invitations')"
+          />
+          <localized-link
+            :to="{ name: 'Friends' }"
+            class="nav-link text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            v-text="$t('pages.user.navigation.friends')"
+          />
+          <localized-link
+            :to="{ name: 'Profile' }"
+            class="nav-link text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            v-text="$t('pages.user.navigation.profile')"
+          />
+        </div>
+        <div class="pt-4 pb-3 border-t border-gray-700">
+          <div class="flex items-center px-4 sm:px-6 lg:px-8">
+            <div class="flex-shrink-0 bg-gray-800 p-1 rounded-full text-gray-400">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                />
+              </svg>
+            </div>
+            <div class="ml-3">
+              <div class="text-base font-medium leading-none text-white">Tom Cook</div>
+            </div>
 
-          <button
-            type="button"
-            class="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-yellow-500"
-          >
-            <localized-link to="/user/notifications">
+            <button
+              type="button"
+              class="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-yellow-500"
+            >
               <span class="sr-only">{{ $t('pages.user.navigation.notifications') }}</span>
               <svg
                 class="h-6 w-6"
@@ -218,23 +224,23 @@
                   d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                 />
               </svg>
-            </localized-link>
-          </button>
-        </div>
-        <div class="mt-3 px-2 space-y-1">
-          <localized-link
-            to="/user/settings"
-            class="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700"
-            v-text="$t('pages.user.navigation.settings')"
-          />
-          <localized-link
-            to="/"
-            class="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700"
-            v-text="$t('pages.user.navigation.sing-out')"
-          />
+            </button>
+          </div>
+          <div class="mt-3 px-2 space-y-1">
+            <localized-link
+              :to="{ name: 'Settings' }"
+              class="nav-link block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700"
+              v-text="$t('pages.user.navigation.settings')"
+            />
+            <localized-link
+              :to="{ name: 'Homepage' }"
+              class="nav-link block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700"
+              v-text="$t('pages.user.navigation.sing-out')"
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </transition>
   </nav>
 </template>
 
@@ -247,3 +253,10 @@ export default {
   })
 }
 </script>
+
+<style>
+  nav .nav-link.router-link-active {
+    --tw-bg-opacity: 1;
+    background-color: rgba(17, 24, 39, var(--tw-bg-opacity));
+  }
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -23,26 +23,32 @@ module.exports = [
     children: [
       {
         path: "categories",
+        name: 'Categories',
         component: () => import("../components/UserInterface/MainSections/Categories"),
       },
       {
         path: "invitations",
+        name: 'Invitations',
         component: () => import("../components/UserInterface/MainSections/Invitations"),
       },
       {
         path: "friends",
+        name: 'Friends',
         component: () => import("../components/UserInterface/MainSections/Friends"),
       },
       {
         path: "inviting-to-friends",
+        name: 'Inviting-to-friends',
         component: () => import("../components/UserInterface/MainSections/InvitingToFriends"),
       },
       {
         path: "profile",
+        name: 'Profile',
         component: () => import("../components/UserInterface/MainSections/Profile"),
       },
       {
         path: "settings",
+        name: 'Settings',
         component: () => import("../components/UserInterface/MainSections/Settings"),
       },
     ]
