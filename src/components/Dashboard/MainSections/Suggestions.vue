d2031907b70db094442f8de8f442a2c17a661b71
5david5 <48356242+5david5@users.noreply.github.com>
2021-05-07 01:02:12 +0200

    #47 - Suggestion integration

--- src/components/Dashboard/MainSections/Suggestions.vue
+++ src/components/Dashboard/MainSections/Suggestions.vue
@@ -13,10 +13,6 @@
               <tr>
                 <th scope="col"
                     class="px-2 sm:px-4 md:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    v-text="$t('pages.dashboard.suggestions-panel.category')"
-                />
-                <th scope="col"
-                    class="px-2 sm:px-4 md:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                     v-text="$t('pages.dashboard.suggestions-panel.name')"
                 />
                 <th scope="col"
@@ -26,107 +22,101 @@
               </tr>
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
-              <tr v-for="suggestion in suggestions" :key="suggestion" class="hover:bg-gray-100">
-                <td class="px-2 sm:px-4 md:px-6 py-4 whitespace-nowrap">
+              <tr v-for="suggestion in suggestions.data" :key="suggestion" class="hover:bg-gray-100">
+                <td class="px-2 sm:px-4 md:px-6 py-4">
                   <div class="flex items-center">
-                    <div class="text-sm font-medium text-gray-900" v-text="suggestion.name" />
+                    <div class="text-sm font-medium text-gray-900" v-text="suggestion.text" />
                   </div>
                 </td>
-                <td class="px-2 sm:px-4 md:px-6 py-4 whitespace-nowrap">
-                  <div class="flex items-center">
-                    <div class="text-sm font-medium text-gray-900" v-text="suggestion.description" />
+                <td>
+                  <div class="flex px-2 sm:px-6 text-right">
+                    <button type="button"
+                            class="p-1 sm:p-2 text-center text-white transition bg-yellow-600 rounded-full shadow ripple hover:shadow-lg hover:bg-yellow-700 focus:outline-none"
+                            @click="showUpdateModal(suggestion)"
+                    >
+                      <svg xmlns="http://www.w3.org/2000/svg"
+                           class="w-4 sm:w-5 h-4 sm:h-5"
+                           fill="none"
+                           viewBox="0 0 24 24"
+                           stroke="currentColor"
+                      >
+                        <path stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="2"
+                              d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+                        />
+                      </svg>
+                    </button>
                   </div>
-                </td>
-                <td class="px-2 sm:px-4 md:px-6 py-4 whitespace-nowrap text-right text-sm font-medium space-x-2">
-                  <button type="button"
-                          class="inline-block p-1 sm:p-2 text-center text-white transition bg-green-600 rounded-full shadow ripple hover:shadow-lg hover:bg-green-700 focus:outline-none"
-                          @click="isEditModalVisible=true"
-                  >
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                         class="w-4 sm:w-5 h-4 sm:h-5"
-                         fill="none"
-                         viewBox="0 0 24 24"
-                         stroke="currentColor"
-                    >
-                      <path stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                      />
-                    </svg>
-                  </button>
-                  <button type="button"
-                          class="inline-block p-1 sm:p-2 text-center text-white transition bg-red-500 rounded-full shadow ripple hover:shadow-lg hover:bg-red-600 focus:outline-none"
-                          @click="isDeleteModalVisible=true"
-                  >
-                    <svg class="w-4 sm:w-5 h-4 sm:h-5 text-white"
-                         xmlns="http://www.w3.org/2000/svg"
-                         viewBox="0 0 20 20"
-                         fill="currentColor"
-                    >
-                      <path fill-rule="evenodd"
-                            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                            clip-rule="evenodd"
-                      />
-                    </svg>
-                  </button>
                 </td>
               </tr>
             </tbody>
           </table>
-          <Pagination />
+          <Pagination @previous-page="previousPage()"
+                      @next-page="nextPage()"
+          />
         </div>
       </div>
     </div>
-    <EditSuggestion v-show="isEditModalVisible"
-                    @close="isEditModalVisible=false"
-    />
-    <DeleteModal v-show="isDeleteModalVisible"
-                 :tittle="$t('pages.dashboard.suggestions-panel.removal.tittle')"
-                 :question="$t('pages.dashboard.suggestions-panel.removal.question')"
-                 @close="isDeleteModalVisible=false"
+    <EditSuggestion v-if="currentSuggestion && this.isEditModalVisible"
+                    :suggestion-id="currentSuggestion.id"
+                    @close="this.isEditModalVisible = false"
+                    @edit="loadPage()"
     />
   </main>
 </template>
-
 <script>
+import {mapGetters} from "vuex"
 import Pagination from "../Pagination"
-import DeleteModal from "../Modals/DeleteModal"
 import EditSuggestion from "../Modals/EditSuggestion"
 
 export default {
   name: "Suggestions",
 
   components: {
-    Pagination,
     EditSuggestion,
-    DeleteModal
+    Pagination,
+  },
+
+  computed: {
+    ...mapGetters(["suggestions"]),
+  },
+
+  methods: {
+    loadPage() {
+      this.$store.dispatch("GET_SUGGESTIONS", this.currentPage);
+    },
+
+    previousPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+        this.loadPage();
+      }
+    },
+
+    nextPage() {
+      if (this.currentPage < this.suggestions.pagination.total_pages) {
+        this.currentPage++;
+        this.loadPage();
+      }
+    },
+
+    showUpdateModal(currentSuggestion) {
+      this.currentSuggestion = currentSuggestion;
+      this.isEditModalVisible = true;
+    },
   },
 
   data() {
     return {
       isEditModalVisible: false,
-      isDeleteModalVisible: false,
-
-      suggestions: [
-        {
-          name: "Kategoria - 1",
-          description: "Opis Pytania 1"
-        },
-        {
-          name: "Kategoria - 1",
-          description: "Opis Pytania 2"
-        },
-        {
-          name: "Kategoria - 1",
-          description: "Opis Pytania 3"
-        },
-        {
-          name: "Kategoria - 2",
-          description: "Opis Pytania 4"
-        },
-      ]
+      currentPage: 1,
+      currentSuggestion: null,
     }
-  }
+  },
+
+  mounted() {
+    this.loadPage();
+  },
 }
 </script>
